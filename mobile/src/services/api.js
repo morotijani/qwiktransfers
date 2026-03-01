d8f8a878ba477cc35fa78e2a1781ababf4950431
Tijani Moro <tjhackx111@gmail.com>
2026-03-01 13:11:54 +0000

    bug fix

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // Update this to your computer's local IP address or Ngrok URL for mobile testing
 // Example: 'http://192.168.1.10:5000/api' or 'https://your-ngrok.ngrok-free.app/api'
 // const API_URL = 'http://localhost:5000/api';
-const API_URL = 'http://192.168.210.98:5000/api';
+const API_URL = 'http://192.168.0.188:5000/api';
 
 const api = axios.create({
     baseURL: API_URL,
